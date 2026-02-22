09226193be8ceb709eb1e3c12f644b4648a51329
rron.aliu <rron.aliu@g2m-ext.com>
2026-02-22 13:09:08 +0100

    remove ci

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-const basePath = process.env.VITE_BASE_PATH || '/'
-
 export default defineConfig({
-  base: basePath,
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
